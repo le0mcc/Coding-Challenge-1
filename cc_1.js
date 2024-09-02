0b7ba1ba4d50f22b02fee222d40924ef32133038
Leo McClellan <mcclellan2@usf.edu>
2024-09-01 22:55:29 -0400

    Task 5 - Access Control System

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,8 @@
 // Task 1: Variables and Data Types
 
-let employeeName = "Vanessa"
-const employeeID = 1
-var isActive = true
+let employeeName = "Vanessa";
+const employeeID = 1;
+var isActive = true;
 
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
@@ -10,29 +10,40 @@ console.log(isActive, typeof(isActive));
 
 // Task 2: Compound Data Types
 
-let products = ["Pen", "Pencil", "Marker"]
+let products = ["Pen", "Pencil", "Marker"];
 const productDetails = {
         name: products,
         price: "$5",
         inStock: "Yes",
 }
 
-console.log(products, productDetails)
+console.log(products, productDetails);
 
 // Task 3: Assignment Operators
 
-let accountBalance = 49.28
-    accountbalance *= 2
-    accountBalance += 5.99
-    accountBalance -= 5.55
-    accountBalance /= 3
-    accountBalance %= 33
+let accountBalance = 49.28;
+    accountbalance *= 2;
+    accountBalance += 5.99;
+    accountBalance -= 5.55;
+    accountBalance /= 3;
+    accountBalance %= 33;
 
 console.log(accountBalance);
 
 // Task 4: Comparison Operators
 
-let employeeScore1 = 9
-let employeeScore2 = 8
+let employeeScore1 = 9;
+let employeeScore2 = 8;
 
 console.log(employeeScore1 > employeeScore2);
+
+// Task 5: Logical Operators
+
+hasKeyCard = true;
+hasPermission = true;
+
+hasAccess = hasKeyCard && hasPermission;
+noAccess = hasKeyCard || hasPermission;
+hasAccess != noAccess
+
+console.log(hasAccess, noAccess);
