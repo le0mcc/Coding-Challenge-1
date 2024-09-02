f0f4e774108cc43457204cd6057359969d6ba0fc
Leo McClellan <mcclellan2@usf.edu>
2024-09-01 22:13:58 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,14 @@ var isActive = true
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+//Task 2: Compound Data Types
+
+let products = ["Pen", "Pencil", "Marker"]
+const productDetails = {
+        name: products,
+        price: "$5",
+        inStock: "Yes",
+}
+
+console.log(products, productDetails)
